332673c94b60308cba99d675500fca8999f83cb4
rzca <142755153+rzca@users.noreply.github.com>
2023-12-11 23:05:15 -0500

    switch to leaflet ugh

--- middle/src/App.tsx
+++ middle/src/App.tsx
@@ -1,62 +1,90 @@
 import { useEffect, useState } from "react";
-import { Map, Marker, ZoomControl } from "pigeon-maps"
-import { default as permits } from "./data/geocodedpermits.json"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
+
+import { default as permits } from "./data/geocodedpermits.json";
 
 type Permit = {
-  status: string,
-  address: string,
-  units: number,
-  zip: number,
-}
+  status: string;
+  address: string;
+  units: number;
+  zip: number;
+};
 
 type GeocodedPermit = {
-  permit: Permit,
-  location: { latitude: number, longitude: number, }
-}
+  permit: Permit;
+  location: { latitude: number; longitude: number };
+};
 
 export const App = () => {
-
   permits satisfies GeocodedPermit[];
 
   const [activePermit, setActivePermit] = useState<GeocodedPermit | null>(null);
-  const [geocodedPermits, setGeocodedPermits] = useState<GeocodedPermit[] | null>(null);
+  const [geocodedPermits, setGeocodedPermits] = useState<
+    GeocodedPermit[] | null
+  >(null);
   const [errorString, setErrorString] = useState<string | null>(null);
 
   useEffect(() => {
-    document.title = "EHO Map"
+    document.title = "EHO Map";
     try {
       setGeocodedPermits(permits);
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err);
       setErrorString("failed to parse data");
     }
   }, []);
 
   return (
-    <div style={{ position: 'absolute', left: 10, top: 10 }}>
-
-      <Map
-        height={Math.min(window.innerHeight - 100, 600)} width={Math.min(window.innerWidth - 20, 1000)}
-        defaultCenter={[38.89, -77.11,]} defaultZoom={11}>
-        {geocodedPermits && geocodedPermits.map(p => {
-          return <Marker
-            anchor={[p.location?.longitude, p.location?.latitude]}
-            onMouseOver={_ => setActivePermit(p)}
-            onMouseOut={_ => setActivePermit(null)}
-          ></Marker>
-        })}
-        <ZoomControl />
-      </Map>
-      {activePermit && <div>
-        <div>{activePermit.permit.address}</div>
-        <div>{activePermit.permit.units + " units"}</div>
-        <div>{activePermit.permit.status}</div>
-      </div>}
+    <div>
+      <div>
+        <MapContainer
+          center={[38.89, -77.11]}
+          zoom={11}
+          scrollWheelZoom={true}
+          style={{ height: "80vh", width: "100vh" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {geocodedPermits &&
+            geocodedPermits.map((p) => {
+              return (
+                <div
+                  // onMouseOver={(_) => setActivePermit(p)}
+                  // onMouseOut={(_) => setActivePermit(null)}
+                  onClick={(_) => setActivePermit(p)}
+                >
+                  <Marker
+                    position={[p.location.longitude, p.location.latitude]}
+                    eventHandlers={{
+                      mouseover: (_) => setActivePermit(p),
+                    }}
+                  >
+                    <div>{p.permit.address}</div>
+                    <Popup>
+                      <div>{p.permit.address}</div>
+                      <div>{p.permit.units + " units"}</div>
+                      <div>{p.permit.status}</div>
+                    </Popup>
+                  </Marker>
+                </div>
+              );
+            })}
+        </MapContainer>
+      </div>
+      {activePermit && (
+        <div>
+          <div>{activePermit.permit.address}</div>
+          <div>{activePermit.permit.units + " units"}</div>
+          <div>{activePermit.permit.status}</div>
+        </div>
+      )}
       <div style={{}}>{errorString}</div>
     </div>
   );
-}
+};
 
 export default App;
