2c7fbefb8f3a5e246513cd883178487b30ad06b0
rzca <142755153+rzca@users.noreply.github.com>
2023-12-12 22:51:55 -0500

    add arlington borders and styling

--- middle/src/App.tsx
+++ middle/src/App.tsx
@@ -1,27 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "leaflet/dist/leaflet.css";
-import 'leaflet/dist/images/marker-icon-2x.png'
-import "leaflet/dist/images/marker-icon.png"
-import "leaflet/dist/images/marker-shadow.png"
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
 import "./App.css";
 
 import { default as permits } from "./data/geocodedpermits.json";
+import { default as arlington } from "./data/arlington.json";
 
+// these are some hacks to get markers to show up correctly in leaflet
 // https://github.com/Leaflet/Leaflet/issues/4968#issuecomment-264311098
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+// import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import L from "leaflet";
+import { Section } from "@blueprintjs/core";
 
-import L from 'leaflet';
+const ActiveIcon = L.icon({
+  // iconUrl: icon,
+  iconUrl:
+    "https://mapmarker.io/api/v3/font-awesome/v6/pin?icon=fa-solid%20fa-house&size=75&color=FFF&background=e97451&hoffset=0&voffset=0",
+  shadowUrl: iconShadow,
+  iconSize: [40, 50],
+  iconAnchor: [12, 36],
+});
 
 const DefaultIcon = L.icon({
-   iconUrl: icon,
-   shadowUrl: iconShadow,
-   iconSize: [24,36],
-   iconAnchor: [12,36]
- });
-
- L.Marker.prototype.options.icon = DefaultIcon; 
+  // iconUrl: icon,
+  iconUrl:
+    "https://mapmarker.io/api/v3/font-awesome/v6/pin?icon=fa-solid%20fa-house&size=75&color=FFF&background=003e77&hoffset=0&voffset=0",
+  shadowUrl: iconShadow,
+  iconSize: [30, 40],
+  iconAnchor: [12, 36],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+// end hacks
 
 type Permit = {
   status: string;
@@ -45,7 +55,6 @@ export const App = () => {
   const [errorString, setErrorString] = useState<string | null>(null);
 
   useEffect(() => {
-    document.title = "EHO Map";
     try {
       setGeocodedPermits(permits);
     } catch (err) {
@@ -54,47 +63,101 @@ export const App = () => {
     }
   }, []);
 
+  const mapContainer = useMemo(() => {
+    return (
+      <MapContainer
+        center={[38.89, -77.11]}
+        zoom={11}
+        scrollWheelZoom={true}
+        style={{
+          margin: "auto",
+          height: "80vh",
+          width: "100vw",
+          maxWidth: "1200px",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* @ts-expect-error idk why this it doesn't like this*/}
+        <GeoJSON data={arlington as GeoJSONObject}></GeoJSON>
+
+        {geocodedPermits &&
+          geocodedPermits.map((p) => {
+            return (
+              <Marker
+                icon={
+                  p.permit.address === activePermit?.permit.address
+                    ? ActiveIcon
+                    : DefaultIcon
+                }
+                position={[p.location.longitude, p.location.latitude]}
+                eventHandlers={{
+                  mouseover: (_) => setActivePermit(p),
+                }}
+                key={p.permit.address}
+              >
+                <Popup>
+                  <div>{p.permit.address}</div>
+                  <div>{p.permit.units + " units"}</div>
+                  <div>{p.permit.status}</div>
+                </Popup>
+              </Marker>
+            );
+          })}
+      </MapContainer>
+    );
+  }, [geocodedPermits, activePermit]);
+
   return (
     <div>
       <div>
-        <MapContainer
-          center={[38.89, -77.11]}
-          zoom={11}
-          scrollWheelZoom={true}
-          style={{ margin: "auto", height: "80vh", width: "85vw", maxWidth: "1200px"}}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {geocodedPermits &&
-            geocodedPermits.map((p) => {
-              return (
-                  <Marker
-                    position={[p.location.longitude, p.location.latitude]}
-                    eventHandlers={{
-                      mouseover: (_) => setActivePermit(p),
-                    }}
-                    key={p.permit.address}
-                  >
-                    <Popup>
-                      <div>{p.permit.address}</div>
-                      <div>{p.permit.units + " units"}</div>
-                      <div>{p.permit.status}</div>
-                    </Popup>
-                  </Marker>
-              );
-            })}
-        </MapContainer>
+        <Section>{mapContainer}</Section>
+        <Section>
+          {activePermit && (
+            <div>
+              <div>{activePermit.permit.address}</div>
+              <div>{activePermit.permit.units + " units"}</div>
+              <div>{activePermit.permit.status}</div>
+            </div>
+          )}
+          <div style={{}}>{errorString}</div>
+          {geocodedPermits && (
+            <table className="bp5-html-table {{.modifier}}">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Units</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {geocodedPermits.map((p) => (
+                  <tr onMouseOver={(_) => setActivePermit(p)}>
+                    <td>{p.permit.address}</td>
+                    <td>{p.permit.units}</td>
+                    <td>{}</td>
+                    <td>{p.permit.status}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </Section>
+        <Section>
+          <div>
+            Credit to{" "}
+            <a href="https://cartographyvectors.com/map/1129-virginia-with-county-boundaries" >https://cartographyvectors.com/map/1129-virginia-with-county-boundaries</a>{" "}
+            for the outline of Arlington
+          </div>
+          <div>
+            Original data from{" "}
+            <a href="https://www.arlingtonva.us/Government/Programs/Building/Permits/EHO/Tracker">https://www.arlingtonva.us/Government/Programs/Building/Permits/EHO/Tracker</a>
+          </div>
+          <div>Geocoding accuracy is best effort, not guaranteed</div>
+        </Section>
       </div>
-      {activePermit && (
-        <div>
-          <div>{activePermit.permit.address}</div>
-          <div>{activePermit.permit.units + " units"}</div>
-          <div>{activePermit.permit.status}</div>
-        </div>
-      )}
-      <div style={{}}>{errorString}</div>
     </div>
   );
 };
